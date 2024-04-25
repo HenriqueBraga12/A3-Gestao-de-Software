f7447db2bb8c8f68f987f49d305c6fe5e7a0906a
MarCCola <marguikrgs@hotmail.com>
2024-04-25 02:23:19 -0300

    feat: adicionando configuraç~ões restante do banco

---
+++ A3_Gestao/ProjetoA3/A3/target/test-classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_imc (id, imc, classificacao)
+VALUES
+    (18.5, 1,  'Baixo peso'),
+    (24.9, 2,  'Peso normal'),
+    (29.9, 3,  'Excesso de peso'),
+    (34.9, 4,  'Obesidade de Classe 1'),
+    (39.9, 5, 'Obesidade de Classe 2'),
+    (40.0, 6, 'Obesidade de Classe 3');
