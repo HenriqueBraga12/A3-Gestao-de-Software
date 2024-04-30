62c09aee3a129619050a304969c1d7a34ac24f98
deveju <117952692+deveju@users.noreply.github.com>
2024-04-30 16:29:34 -0300

    Update ImcValueDto.java
    Adição dos métodos 'get' para o valor do IMC e para a classificação do IMC

--- projetoA3/src/main/java/com/imc/projetoA3/dto/ImcValueDto.java
+++ projetoA3/src/main/java/com/imc/projetoA3/dto/ImcValueDto.java
@@ -1,5 +1,13 @@
 package com.imc.projetoA3.dto;
 
-public record ImcValueDto (Double imc,
-                           String classificacao) {
+public record ImcValueDto (Double imc, String classificacao) {
+
+    public double getImcValue() {
+        // TODO Auto-generated method stub
+        return imc;
+    }
+
+    public String getImcClass() {
+        return classificacao;
+    }
 }
