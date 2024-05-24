503f82103ccc9bbff61fb695b4c7fba2f9ea0c3e
Marcos Cruz <128255034+Marccosx@users.noreply.github.com>
2024-05-24 11:13:05 -0300

    feat : update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -3,18 +3,20 @@
 # Configurações FTP
 FTP_SERVER="joaobsjunior.com.br"
 FTP_USER="aluno-ftp"
-FTP_PASS="a1b2c3d4@"
+FTP_PASS=a1b2c3d4@"
 FTP_DIR="A3-Gestao-de-Software"
 
 # Diretório local dos relatórios gerados pelo JaCoCo
-LOCAL_DIR="/projetoA3/target/site/jacoco/"
+LOCAL_DIR="./target/site/jacoco/"
 
 # Comando para gerar os relatórios com o JaCoCo
-./gradlew jacocoTestReport  # Substitua pelo comando correto para gerar os relatórios
+mvn jacoco:report
 
-# Comando para enviar os relatórios para o servidor FTP
-cd "$LOCAL_DIR" || exit
-ftp -n "$FTP_SERVER" <<END_SCRIPT
+# Verifica se o diretório de relatórios existe
+if [ -d "$LOCAL_DIR" ]; then
+  # Comando para enviar os relatórios para o servidor FTP
+  cd "$LOCAL_DIR" || exit
+  ftp -n "$FTP_SERVER" <<END_SCRIPT
 quote USER "$FTP_USER"
 quote PASS "$FTP_PASS"
 mkdir "$FTP_DIR"
@@ -22,3 +24,7 @@ cd "$FTP_DIR"
 mput *
 quit
 END_SCRIPT
+else
+  echo "Diretório de relatórios não encontrado: $LOCAL_DIR"
+  exit 1
+fi
